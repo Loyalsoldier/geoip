29473762b9978ee35e56457c65a0ba937fb12a74
Darien Raymond <admin@v2ray.com>
2018-12-11 12:09:39 +0100

    fix path to main

--- generate.sh
+++ generate.sh
@@ -8,4 +8,4 @@ rm GeoLite2-Country-CSV.zip
 mv GeoLite2* geoip
 
 CGO_ENABLED=0 go get -u v2ray.com/ext/tools/geoip/main
-main --country=./geoip/GeoLite2-Country-Locations-en.csv --ipv4=./geoip/GeoLite2-Country-Blocks-IPv4.csv --ipv6=./geoip/GeoLite2-Country-Blocks-IPv6.csv
+${GOPATH}/bin/main --country=./geoip/GeoLite2-Country-Locations-en.csv --ipv4=./geoip/GeoLite2-Country-Blocks-IPv4.csv --ipv6=./geoip/GeoLite2-Country-Blocks-IPv6.csv
