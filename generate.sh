44ed55c7778c940638cbd64544ff239794d8d847
Darien Raymond <admin@v2ray.com>
2019-01-18 17:22:35 +0100

    update path

--- generate.sh
+++ generate.sh
@@ -6,9 +6,10 @@ curl -L -O http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country-C
 unzip GeoLite2-Country-CSV.zip
 rm GeoLite2-Country-CSV.zip
 mv GeoLite2* geoip
+ls ./geoip
 
-go get -v -t -d github.com/v2ray/geoip/...
-go run $GOPATH/src/github.com/v2ray/geoip/main.go -- --country=./geoip/GeoLite2-Country-Locations-en.csv --ipv4=./geoip/GeoLite2-Country-Blocks-IPv4.csv --ipv6=./geoip/GeoLite2-Country-Blocks-IPv6.csv
+go get -u github.com/v2ray/geoip
+$GOPATH/bin/geoip --country=./geoip/GeoLite2-Country-Locations-en.csv --ipv4=./geoip/GeoLite2-Country-Blocks-IPv4.csv --ipv6=./geoip/GeoLite2-Country-Blocks-IPv6.csv
 
 mkdir ./publish
 mv ./geoip.dat ./publish/
