726da4135fc679118c4007e1d792e6d743341a3d
Darien Raymond <admin@v2ray.com>
2019-03-01 00:12:01 +0100

    use official protobuf lib

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"os"
 	"strings"
 
-	"github.com/gogo/protobuf/proto"
+	"github.com/golang/protobuf/proto"
 	"v2ray.com/core/app/router"
 	"v2ray.com/core/common"
 	"v2ray.com/core/infra/conf"
